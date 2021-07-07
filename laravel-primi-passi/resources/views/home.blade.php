8eb7d342b88a15977f2997fbc5c48334952b5403
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-07-07 15:55:12 +0200

    Used foreah loop to loop through data given to view

--- laravel-primi-passi/resources/views/home.blade.php
+++ laravel-primi-passi/resources/views/home.blade.php
@@ -4,9 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="">
     <title>Home</title>
 </head>
+
+<style>
+*{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+body{
+    display: flex;
+    align-items: center;
+    justify-content:center;
+    margin-top: 3rem;
+}
+</style>
 <body>
-    <h1>HOME</h1>
+    <div>
+        @foreach($laptops as $laptop)
+            <span>{{$laptop}}</span>
+            @if(!$loop->last)
+                -
+            @endif
+        @endforeach
+    </div>
 </body>
 </html>
--- laravel-primi-passi/routes/web.php
+++ laravel-primi-passi/routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $products = [
+        'laptops'=>[
+            'Mackbook',
+            'Acer',
+            'Asus',
+            'HP'
+            ]
+    ];
+    return view('home', $products);
 });
